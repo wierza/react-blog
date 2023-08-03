4181e25185f05b5a244816e100f29af8b72ecf5c
wierza <dawid@wierzycki.pl>
2023-08-03 20:42:33 +0200

    Add Footer and Navbar styles

--- src/components/views/Footer.js
+++ src/components/views/Footer.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <p>Footer</p>
+        <div className="text-center text-muted my-4">
+            Copyright &copy; BlogApp 2023
+        </div>
     )
 };
 
--- src/components/views/NavBar.js
+++ src/components/views/NavBar.js
@@ -1,8 +1,23 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
     return (
-        <p>NavBar</p>
+        <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+            <Container>
+                <Navbar.Brand as={NavLink} to="/"> Blog.app </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>    
+        </Navbar>
     )
 };
 
