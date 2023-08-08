9df6f152d466521e747afde95acef744a33e58ea
wierza <dawid@wierzycki.pl>
2023-08-08 17:18:29 +0200

    Add edit post

---
+++ src/components/features/EditPostForm.js
@@ -0,0 +1,27 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams, Navigate } from 'react-router-dom';
+import {getPostById, editPost } from '../../redux/postsRedux';
+import PostForm from '../features/PostForm';
+
+const EditPostForm = () => {
+    const disptach = useDispatch();
+    const navigate = useNavigate();
+    const { postId } = useParams();
+    const postData = useSelector(state => getPostById(state, postId));
+    const handleSubmit = post => {
+        disptach(editPost({ ...post, postId: postId }));
+        navigate('/')
+    }
+    if (!postData) return <Navigate to="/" />;
+    return (
+        <PostForm action={handleSubmit} 
+        actionText="Edit post" 
+        title={postData.title}
+        author={postData.author}
+        publishedDate={postData.publishedDate}
+        shortDescription={postData.shortDescription}
+        content={postData.content}/>
+    );
+};
+
+export default EditPostForm;
--- src/components/pages/EditPosts.js
+++ src/components/pages/EditPosts.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import EditPostForm from "../features/EditPostForm";
 
 const EditPost = () => {
     return (
-        <h2> Edit Post </h2>
+        <Row className="justify-content-center">
+            <Col xs="12" md="10" lg="8">
+            <h2> Edit Post </h2>
+            <EditPostForm />
+            </Col>
+        </Row>
+        
     );
 };
 
